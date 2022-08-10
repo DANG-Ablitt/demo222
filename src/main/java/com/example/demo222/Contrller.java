80d7c85c27ebc12dc70d5cae836ca7a6a637b687
liyang <liyang@gmail.com>
2022-08-10 17:30:22 +0800

    测试

---
+++ src/main/java/com/example/demo222/Contrller.java
@@ -0,0 +1,15 @@
+package com.example.demo222;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sys/user")
+public class Contrller {
+
+    @RequestMapping("/info")
+    public String pp(){
+        return "555555";
+    }
+}
